8a22833a8df77180151ae3cbddc58465261e7518
Sophia Manodori <smanodori@smith.edu>
2023-02-14 18:06:48 -0500

    Final Version

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,79 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
+class Conversation {
+  public static String mirrorWords(String sentence){
+      //break sentence into string[] of words 
+      sentence = sentence.toLowerCase();
+      String[] words = sentence.split(" ");
+      // loop over all the words in string
+      String[] initialWords = {"i", "you", "my", "am", "your", "me"};
+      String[] replacement={"you", "I", "your", "are", "my", "you"};
+      String newSentence= "";
+
+      boolean replace = false;
+      //replaces mirror words
+      for (int i = 0; i<words.length; i++){
+        replace= false;
+        if(i!=0){newSentence+=" ";}
+        for(int j = 0; j<initialWords.length; j++){
+        //if(i!=0){newSentence+=" ";}
+        //System.out.println("We're comparing these two things");
+        //System.out.print(words[i]);
+        //System.out.print(initialWords[j]);
+        if(words[i].equals(initialWords[j])){newSentence+= replacement[j]; replace = true;}
+        //else{newSentence += words[i]; break;}
+      }
+      if(!replace){newSentence += words[i];}
+    }
+      String finalSentence = newSentence.substring(0, 1).toUpperCase() + newSentence.substring(1);
+      //for (int i = 0; i<words.length ; i++){
+          //check for i 
+        //  if(i!=0){newSentence += " ";}
+          //if (words[i].equals("i")){
+          //  words[i]= "you"; newSentence+="you";}
+          //if (words[i].equals("my")){words[i]="your"; newSentence+="your";}
+          //if (words[i].equals("you")){words[i]="i"; newSentence+="your";}
+          //else{newSentence +=words[i];}
+      //}
+      return finalSentence;
+  }
   public static void main(String[] arguments) {
     // You will start the conversation here.
+    ArrayList<String> transcript = new ArrayList<String>();
+    String[] generic =  {"That's pretty weird.", "Alright then.", "You should go to a doctor for that.", "Uh-huh."};
+    //asks for rounds
+    int rounds = -1;
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Enter a number of rounds");
+    rounds = userInput.nextInt();
+    userInput.nextLine(); //clearing input
+    System.out.println("Hello! How are you?");
+    Random random= new Random();
+
+    //starts conversation
+      for (int i = 0; i < rounds; i++ ) {
+          String response = userInput.nextLine();
+          transcript.add(response);
+          String answer = mirrorWords(response);//replacemirrorwords //generic[random.nextInt(generic.length+1)];
+          if(answer.toUpperCase().equals(response.toUpperCase())){ 
+            answer = generic[random.nextInt(generic.length)];
+          }
+          else{answer = answer+="?";}
+          transcript.add(answer);
+          System.out.println(answer);
+            }
+      userInput.close();
+
+
+      //prints transcript 
+      System.out.println("Transcript");
+      for (int i = 0; i < transcript.size(); i++){
+        if (i%2==0){
+        System.out.println("You: " + transcript.get(i)); }
+        if (i%2 ==1){
+          System.out.println("Computer: "+ transcript.get(i)); }
+      }
+        }
   }
-}
--- reflection.md
+++ reflection.md
@@ -1,7 +1,11 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment?
-- Which session(s) of TA / office hours did you attend?
+- Which classmates did you consult with while working on this assignment? 
+I did not.
+- Which session(s) of TA / office hours did you attend? 
+I attended the friday morning office hours with Jordan.
 - What are your initial impressions of Java? 
-- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+I kind of like it, but it's definitely a difficult transition from Python. It's very picky and so you have to be super careful about what you put down. 
+- Can you draw any conclusion about programming in general from the similarities or the differences between the two languages?  I think java in general has more steps to program something, like to do something simple I feel like you have to do way more steps and take way more precautions in java. I also don't really understand the class-method-object stuff, but I think that's just trial and error. 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+I would recommend taking an online intro to java course online for like a week or two just so when you get a big assignment like this, you don't have to google as much, because I had to do a lot of documentation research which, I've discovered, is really difficult with java and very confusing. 
