f4db90cb97e1256a756f19c89872007525bb9a17
Sophia Manodori <smanodori@smith.edu>
2023-02-15 17:44:39 -0500

    Made indents better

--- Conversation.java
+++ Conversation.java
@@ -3,6 +3,11 @@ import java.util.ArrayList;
 import java.util.Random;
 
 class Conversation {
+  /**
+   * to mirror the words given by the imput
+   * @param sentence
+   * @return a sentence with correct mirrored words and a question mark
+   */
   public static String mirrorWords(String sentence){
       //break sentence into string[] of words 
       sentence = sentence.toLowerCase();
@@ -22,10 +27,15 @@ class Conversation {
         //System.out.println("We're comparing these two things");
         //System.out.print(words[i]);
         //System.out.print(initialWords[j]);
-        if(words[i].equals(initialWords[j])){newSentence+= replacement[j]; replace = true;}
+        if(words[i].equals(initialWords[j])){
+          newSentence+= replacement[j]; 
+          replace = true;
+        }
         //else{newSentence += words[i]; break;}
       }
-      if(!replace){newSentence += words[i];}
+      if(!replace){
+        newSentence += words[i];
+      }
     }
       String finalSentence = newSentence.substring(0, 1).toUpperCase() + newSentence.substring(1);
       //for (int i = 0; i<words.length ; i++){
@@ -39,6 +49,10 @@ class Conversation {
       //}
       return finalSentence;
   }
+  /**
+   * runs the conversation
+   * @param arguments
+   */
   public static void main(String[] arguments) {
     // You will start the conversation here.
     ArrayList<String> transcript = new ArrayList<String>();
@@ -60,10 +74,12 @@ class Conversation {
           if(answer.toUpperCase().equals(response.toUpperCase())){ 
             answer = generic[random.nextInt(generic.length)];
           }
-          else{answer = answer+="?";}
+          else{
+            answer = answer+="?";
+          }
           transcript.add(answer);
           System.out.println(answer);
-            }
+      }
       userInput.close();
 
 
@@ -71,9 +87,11 @@ class Conversation {
       System.out.println("Transcript");
       for (int i = 0; i < transcript.size(); i++){
         if (i%2==0){
-        System.out.println("You: " + transcript.get(i)); }
-        if (i%2 ==1){
-          System.out.println("Computer: "+ transcript.get(i)); }
+        System.out.println("You: " + transcript.get(i)); 
       }
+        if (i%2 ==1){
+          System.out.println("Computer: "+ transcript.get(i)); 
         }
+      }
+    }
   }
